8b61a171bcbcc2bc5f6cf2bde9ea97ddbefc5f78
cfernando415 <ca.fernando415@gmail.com>
2018-11-20 17:50:42 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,46 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :method_override, true
   end
 
   # code actions here!
+  get "/recipes" do
+    @recipes = Recipe.all
+    erb :index
+  end
 
+  get "/recipes/new" do
+    erb :new
+  end
+
+  post "/recipes" do
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  get "/recipes/:id" do
+    @recipe = Recipe.find(params[:id])
+    erb :recipe
+  end
+
+  get "/recipes/:id/edit" do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  patch "/recipes/:id" do
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(params[:recipe])
+    redirect to "/recipes/#{params[:id]}"
+  end
+
+  delete "/recipes/:id" do
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect to "/recipes"
+  end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,1 +1,9 @@
 #Placeholder for a model
+class Recipe < ActiveRecord::Base
+  # attr_accessor :name, :ingredients, :cook_time
+  # @@all = []
+  # def initialize(name, ingredients, cook_time)
+  #   @name, @ingredients, @cook_time = name, ingredients, cook_time
+  #   @@all << self
+  # end
+end
